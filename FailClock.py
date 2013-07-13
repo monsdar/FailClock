b1cb612efb07ecb0fdabe901003b88dff21fd124
Nils Brinkmann <monsdar+github@gmail.com>
2013-07-13 14:43:43 +0200

    Added: Python script to reset the counter

---
+++ FailClock.py
@@ -0,0 +1,35 @@
+
+import datetime
+import serial
+import time
+
+def getSecondsSinceMidnight():
+    now = datetime.datetime.now()
+    midnight = now.replace(hour=0, minute=0, second=0, microsecond=0)
+    seconds = (now - midnight).seconds
+    return seconds
+    
+def intTo5DigitString(number):
+    return "%05d" % number 
+
+def main():
+    #Note: This assumes that the Arduino is connected to COM3. Usually
+    #      this is the case when there aren't any other Serial-devices.
+    serialHandle = serial.Serial("COM3", 9600)
+
+    #reset the display
+    #Note: this returns the previous value, we're ignoring it for now
+    #Note: We need to sleep a bit before writing to the device
+    time.sleep(2.0)
+    serialHandle.write("R0000")
+
+    #reset the timer
+    #Note: Needs to be done because the serial connection resets the Arduino
+    #      It also comes in practical to sync the timer from time to time...
+    time.sleep(2.0)
+    seconds = getSecondsSinceMidnight()
+    secondStr = intTo5DigitString(seconds)
+    serialHandle.write("T" + secondStr)
+
+if __name__ == "__main__":
+    main()
